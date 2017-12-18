cccf8cdfbc34e13f49735faf6e33b6559c3d324f
Alex Black <alexblack426@gmail.com>
2017-12-18 14:42:20 -0800

    added express router, broke out job and employees

--- db/routes/jobs.js
+++ db/routes/jobs.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = expres.Router();
+const router = express.Router();
 const db = require('../models/index');
 const checkJwt = require('./jwt');
 const jwtAuthz = require('express-jwt-authz');
