24ce5aca84cef2eb0bb05964d99084570d9dd1bb
Alex Black <alexblack426@gmail.com>
2017-12-18 14:39:32 -0800

    added express router, broke out job and employees

--- db/routes/jwt.js
+++ db/routes/jwt.js
@@ -1,3 +1,6 @@
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+
 const checkJwt = jwt({
     // Dynamically provide a signing key based on the kid in the header and the singing keys provided by the JWKS endpoint.
     secret: jwksRsa.expressJwtSecret({
