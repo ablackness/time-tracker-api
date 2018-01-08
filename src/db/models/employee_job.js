7735e487538e593e55e0d8753ff14a1c2318530f
Alex Black <alexblack426@gmail.com>
2018-01-08 14:37:19 -0800

    added employee job table and model

---
+++ src/db/models/employee_job.js
@@ -0,0 +1,55 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('employee_job', {
+    EmployeeJobID: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    EmployeeID: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'employee',
+        key: 'EmployeeID'
+      }
+    },
+    JobID: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'job',
+        key: 'JobID'
+      }
+    },
+    StartTime: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    EndTime: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    created_by: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    created_date: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    modified_by: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    modified_date: {
+      type: DataTypes.DATE,
+      allowNull: true
+    }
+  }, {
+    tableName: 'employee_job',
+    timestamps: false
+  });
+};
